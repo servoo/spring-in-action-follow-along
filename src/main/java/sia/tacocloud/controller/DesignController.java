4902cf70039ffc9e8081737d2e13c97450916fe1
servoo <servettakmaz@gmail.com>
2022-10-01 10:36:13 +0200

    Add logic for controller

--- src/main/java/sia/tacocloud/controller/DesignController.java
+++ src/main/java/sia/tacocloud/controller/DesignController.java
@@ -5,31 +5,43 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import sia.tacocloud.model.Ingredient;
 import sia.tacocloud.model.Ingredient.Type;
+import sia.tacocloud.model.Taco;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("tacoOrder")
 public class DesignController {
 
     @GetMapping
-    public String showDesignForm(Model model) {
-//        List<Ingredient> ingredients = Arrays.asList(
-//                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP)
-//        );
-//
-//        Type[] types = Ingredient.Type.values();
-//        for (Type type : types) {
-//            model.addAttribute(type.toString().toLowerCase(),
-//                    filterByType(ingredients, type));
-//        }
-//
-//        model.addAttribute("design", new Taco());
-//
-        return "design";
+    public void addIngredients(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN)
+        );
+
+        Type[] types = Ingredient.Type.values();
+        for (Type type : Type.values()) {
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+        for (Map.Entry<String, Object> pair : model.asMap().entrySet()) System.out.println(pair);
+
+        model.addAttribute("design", new Taco());
+    }
+
+    private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(ingredient -> ingredient.getType().equals(type))
+                .collect(Collectors.toList());
     }
 }
