5a5ac7f0b65d9fe40a0c884b9a8abff5fcf7a622
servoo <servettakmaz@gmail.com>
2022-10-04 16:20:24 +0200

    Clean code

--- src/main/java/sia/tacocloud/controller/DesignController.java
+++ src/main/java/sia/tacocloud/controller/DesignController.java
@@ -33,7 +33,7 @@ public class DesignController {
         for (Type type : types) {
             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
         }
-        for (Map.Entry<String, Object> pair : model.asMap().entrySet()) System.out.println(pair);
+        model.asMap().entrySet().forEach(System.out::println);
 
         model.addAttribute("design", new Taco());
     }
