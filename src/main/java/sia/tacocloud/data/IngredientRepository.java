a358c879ef68480968dcc192a38cf7ffeae6fde6
servoo <servettakmaz@gmail.com>
2022-10-11 22:30:10 +0200

    Add missing files to commit for prev commit

---
+++ src/main/java/sia/tacocloud/data/IngredientRepository.java
@@ -0,0 +1,14 @@
+package sia.tacocloud.data;
+
+import sia.tacocloud.model.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+
+    Iterable<Ingredient> findAll();
+
+    Optional<Ingredient> findById(String id);
+
+    Ingredient save(Ingredient ingredient);
+}
--- src/main/java/sia/tacocloud/data/JdbcIngredientRepository.java
+++ src/main/java/sia/tacocloud/data/JdbcIngredientRepository.java
@@ -1,22 +1,53 @@
 package sia.tacocloud.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import sia.tacocloud.model.Ingredient;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class JdbcIngredientRepository implements IngredientRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcIngredientRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public Iterable<Ingredient> findAll() {
-        return null;
+        return jdbcTemplate.query(
+                "select id, name, type from Ingredient",
+                this::mapRowToIngredient);
     }
 
     @Override
     public Optional<Ingredient> findById(String id) {
-        return Optional.empty();
+        List<Ingredient> results = jdbcTemplate.query(
+                "select id, name, type from Ingredient where id=?",
+                this::mapRowToIngredient,
+                id);
+        return results.size() == 0 ?
+                Optional.empty() :
+                Optional.of(results.get(0));
     }
 
     @Override
     public Ingredient save(Ingredient ingredient) {
         return null;
+    }
+
+    private Ingredient mapRowToIngredient(ResultSet row, int rowNumber) throws SQLException {
+        return new Ingredient(
+                row.getString("id"),
+                row.getString("name"),
+                Ingredient.Type.valueOf(row.getString("type"))
+        );
     }
 }
