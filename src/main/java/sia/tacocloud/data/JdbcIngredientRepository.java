0f236908f0950e09ffb93d5a3e4c9f2157cb28a3
servoo <servettakmaz@gmail.com>
2022-10-12 21:34:59 +0200

    Implement the save method in JdbcIngredientRepository

--- src/main/java/sia/tacocloud/data/JdbcIngredientRepository.java
+++ src/main/java/sia/tacocloud/data/JdbcIngredientRepository.java
@@ -40,7 +40,13 @@ public class JdbcIngredientRepository implements IngredientRepository {
 
     @Override
     public Ingredient save(Ingredient ingredient) {
-        return null;
+        jdbcTemplate.update(
+                "insert into Ingredient (id, name, type) values (?, ?, ?)",
+                ingredient.getId(),
+                ingredient.getName(),
+                ingredient.getType().toString()
+        );
+        return ingredient;
     }
 
     private Ingredient mapRowToIngredient(ResultSet row, int rowNumber) throws SQLException {
